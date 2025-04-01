4f5a21cd27ef8ea956191db88287f30ec7b856f2
cherry1477 <ehcnah@gmail.com>
2025-04-01 10:17:37 +0900

    first commit

--- Practice1.cpp
+++ Practice1.cpp
@@ -2,105 +2,27 @@
 
 using namespace std;
 
-int main() {
-	// 정수형 변수 선언
-	int num1 = 10, num2 = 4;
 
-	// 사칙연산 출력
-	cout << "입력된 값: num1 = " << num1 << ", num2 = " << num2 << endl;
-	cout << "덧셈 결과: " << num1 + num2 << endl;
-	cout << "뺄셈 결과: " << num1 - num2 << endl;
-	cout << "곱셈 결과: " << num1 * num2 << endl;
+int main()
 
-	// 나눗셈을 할 때 0으로 나누는 경우를 처리
-	if (num2 != 0) {
-		cout << "나눗셈 결과: " << num1 / num2 << endl;
-	}
-	else {
-		cout << "나누기 연산에서 0으로 나눌 수 없습니다!" << endl;
-	}
-
-	return 0;
-}
-
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	// 실수형 변수 선언
-	float num1 = 5.5f, num2 = 2.2f;
-
-	// 사칙연산 출력
-	cout << "입력된 값: num1 = " << num1 << ", num2 = " << num2 << endl;
-	cout << "덧셈 결과: " << num1 + num2 << endl;
-	cout << "뺄셈 결과: " << num1 - num2 << endl;
-	cout << "곱셈 결과: " << num1 * num2 << endl;
-
-	// 나눗셈을 할 때 0으로 나누는 경우를 처리
-	if (num2 != 0) {
-		cout << "나눗셈 결과: " << num1 / num2 << endl;
-	}
-	else {
-		cout << "나누기 연산에서 0으로 나눌 수 없습니다!" << endl;
-	}
-
-	return 0;
-}
+{
+	int Number1 = 10;
+	int Number2 = 20;
 
 
+	cout << (Number1 + Number2) << endl;
+	cout << (Number1 - Number2) << endl;
+	cout << (Number1 / Number2) << endl;
+	cout << (Number1 * Number2) << endl;
 
 
+	float Number3 = 10.5f;
+	float Number4 = 2.5f;
 
-#include <iostream>
-
-using namespace std;
-
-int main() {
-
-
-	// 정수형 변수 선언
-
-
-	//사칙연산 출력
-
-
-	cout << 입력된 값: num1 = " << number1 << "
-	cout << "a, b:"; 
-	
-	cin >> number1 >> number2;
-
-	cout << 
-
-
-	cin >> a;
-	cin >> b;
-	cout << a + b << "\n";
-	cout << a - b << "\n";
-	cout << a * b << "\n";
-	cout << a / b << "\n";
-	cout << a % b << "\n";
-
-	return 0;
-
-}
-
-float NUM_1, NUM_2, RES;
-cout << 
-
-	scanf("%d %d, &a, &b");
-
-
-	printf("%d", a - b);
-
-	printf("%d", a + b);
-
-	printf("%d", a * b);
-
-	printf("%d", a % b);
-
-	printf("%.0lf\n", (double)a / b)
-
+	cout << (Number3 + Number4) << endl;
+	cout << (Number3 - Number4) << endl;
+	cout << (Number3 / Number4) << endl;
+	cout << (Number3 * Number4) << endl;
 
 	return 0;
 
