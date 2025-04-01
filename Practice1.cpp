97fe39b8fa1d144fd711fb51b7eb16e2e3bee394
cherry1477 <ehcnah@gmail.com>
2025-04-01 09:46:47 +0900

    Initial commit

---
+++ Practice1.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// 정수형 변수 선언
+	int num1 = 10, num2 = 4;
+
+	// 사칙연산 출력
+	cout << "입력된 값: num1 = " << num1 << ", num2 = " << num2 << endl;
+	cout << "덧셈 결과: " << num1 + num2 << endl;
+	cout << "뺄셈 결과: " << num1 - num2 << endl;
+	cout << "곱셈 결과: " << num1 * num2 << endl;
+
+	// 나눗셈을 할 때 0으로 나누는 경우를 처리
+	if (num2 != 0) {
+		cout << "나눗셈 결과: " << num1 / num2 << endl;
+	}
+	else {
+		cout << "나누기 연산에서 0으로 나눌 수 없습니다!" << endl;
+	}
+
+	return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// 실수형 변수 선언
+	float num1 = 5.5f, num2 = 2.2f;
+
+	// 사칙연산 출력
+	cout << "입력된 값: num1 = " << num1 << ", num2 = " << num2 << endl;
+	cout << "덧셈 결과: " << num1 + num2 << endl;
+	cout << "뺄셈 결과: " << num1 - num2 << endl;
+	cout << "곱셈 결과: " << num1 * num2 << endl;
+
+	// 나눗셈을 할 때 0으로 나누는 경우를 처리
+	if (num2 != 0) {
+		cout << "나눗셈 결과: " << num1 / num2 << endl;
+	}
+	else {
+		cout << "나누기 연산에서 0으로 나눌 수 없습니다!" << endl;
+	}
+
+	return 0;
+}
+
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+	// 정수형 변수 선언
+
+
+	//사칙연산 출력
+
+
+	cout << 입력된 값: num1 = " << number1 << "
+	cout << "a, b:"; 
+	
+	cin >> number1 >> number2;
+
+	cout << 
+
+
+	cin >> a;
+	cin >> b;
+	cout << a + b << "\n";
+	cout << a - b << "\n";
+	cout << a * b << "\n";
+	cout << a / b << "\n";
+	cout << a % b << "\n";
+
+	return 0;
+
+}
+
+float NUM_1, NUM_2, RES;
+cout << 
+
+	scanf("%d %d, &a, &b");
+
+
+	printf("%d", a - b);
+
+	printf("%d", a + b);
+
+	printf("%d", a * b);
+
+	printf("%d", a % b);
+
+	printf("%.0lf\n", (double)a / b)
+
+
+	return 0;
+
+}
